61808693f5f1e62d403a97cd8570c7b8f18f277d
WilcoApp <hello@wilco.gg>
2023-01-25 03:45:53 +0000

    Funnel Drop

--- frontend/src/components/Item/index.js
+++ frontend/src/components/Item/index.js
@@ -35,9 +35,6 @@ class Item extends React.Component {
     if (!this.props.item) {
       return null;
     }
-    if(!this.props.item.image) {
-      this.props.item.image = '/placeholder.png'
-    }
 
     const markup = {
       __html: marked(this.props.item.description, { sanitize: true }),
--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -20,10 +20,6 @@ const mapDispatchToProps = (dispatch) => ({
 const ItemPreview = (props) => {
   const item = props.item;
 
-  if(!item.image) {
-    item.image = '/placeholder.png'
-  }
-
   const handleClick = (ev) => {
     ev.preventDefault();
     if (item.favorited) {
